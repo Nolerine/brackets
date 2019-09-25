908e72ac1622a4c20089c1b7613eeb90809217c0
Nolerine <nolerine@mail.ru>
2019-09-26 00:41:52 +0300

    feat: implement brackets checker algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  stack = [];
+  for (let char of str) {
+    if (isEqualPair(char, bracketsConfig)) {
+      if (char == stack[stack.length-1]) {                          //обработка непарных скобок
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+      if (isOpeningBracket(char, bracketsConfig)) {                 //обработка парных скобок
+        stack.push(char);
+      } else {
+        if (isPair(stack[stack.length-1], char, bracketsConfig)) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  if (stack.length == 0) {
+    return true;
+  }
+  return false;
+}
+
+function isEqualPair(a, bracketsConfig) {         //проверка на непарность
+  for (let pair of bracketsConfig) {
+    if (a == pair[0] & a == pair[1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isOpeningBracket(a, bracketsConfig) {    //проверка, является ли скобка открывающейся
+  for (let pair of bracketsConfig) {
+    if (a == pair[0]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isPair(a, b, bracketsConfig) {           //проверка разных скобок на парность
+  for (let pair of bracketsConfig) {
+    if (a == pair[0] & b == pair[1]) {
+      return true;
+    }
+  }
+  return false;
 }
